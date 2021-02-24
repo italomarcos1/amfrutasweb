a5d1c91aa91ec307703609e8af8f912e76654dfc
Italo M <italomarcos0010@gmail.com>
2021-02-24 09:46:54 -0300

    another commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import uuid from 'react-uuid';
 
-axios.defaults.baseURL = 'https://sandbox.amfrutas.pt/backend';
+// axios.defaults.baseURL = 'https://sandbox.amfrutas.pt/backend';
+axios.defaults.baseURL = 'https://laravel58.amfrutas.pt/';
 
 axios.interceptors.request.use(async config => {
   const newUuid = uuid();
